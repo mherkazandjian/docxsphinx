77931f2cc776fc91b3e267f94b8603170827f956
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2017-08-30 11:59:03 +0200

    Remove old docx dependencies, add python-docx as dependency.

--- setup.py
+++ setup.py
@@ -38,10 +38,7 @@ setup(
      include_package_data=True,
      install_requires=[
         'Sphinx',
-        #'docx',  # docx is included this package
-        # -*- Extra requirements: -*-
-        'lxml',  # required by docx
-        'pillow',  # required by docx
+        'python-docx',
      ],
      extras_require=dict(
          test=[
