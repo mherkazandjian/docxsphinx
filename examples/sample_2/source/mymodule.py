2053128e945d23bdbef8a8196328f94090c31a0c
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2018-03-27 12:15:28 +0200

    Add autodoc example.

---
+++ examples/sample_2/source/mymodule.py
@@ -0,0 +1,16 @@
+"""
+This is my module.
+"""
+
+
+class MyClass(object):
+    """
+    This is my class.
+    """
+
+    def with_function(self):
+        """
+        With a function
+        :return: MyClass
+        """
+        return self
